6356c422a4912fa454bc783c16c6991ab6365996
Sandip040401 <iemsandip@gmail.com>
2024-09-18 19:48:41 +0530

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -54,6 +54,8 @@ app.get('/temperatures', async (req, res) => {
   res.json(temperatures);
 });
 
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
